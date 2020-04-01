66ecf14b26ee9cd66a92fe76ff348f51b250729e
kotsur-vlad <kotsur.vlad@gmail.com>
2020-04-02 00:20:14 +0300

    divide components, add filterValue, make 'stupid' components, take out data

--- src/App.js
+++ src/App.js
@@ -5,13 +5,23 @@ import TodoListHeader from "./TodoListHeader";
 import TodoListTasks from "./TodoListTasks";
 
 class App extends React.Component {
+
+	tasks = [
+		{title: "JS", isDone: false, priority: "high"},
+		{title: "HTML", isDone: true, priority: "high"},
+		{title: "CSS", isDone: false, priority: "high"},
+		{title: "React", isDone: false, priority: "high"}
+	];
+
+	filterValue = "All";
+
 	render = () => {
 		return (
 			<div className="App">
 				<div className="todoList">
 					<TodoListHeader/>
-					<TodoListTasks/>
-					<TodoListFooter/>
+					<TodoListTasks tasks={this.tasks}/>
+					<TodoListFooter filterValue={this.filterValue}/>
 				</div>
 			</div>
 		);
--- src/TodoListTasks.js
+++ src/TodoListTasks.js
@@ -1,25 +1,16 @@
 import React from "react";
+import TodoListTask from "./TodoListTask";
 
 class TodoListTasks extends React.Component {
 	render = () => {
+
+		let todoListTasks = this.props.tasks.map ((task) => {
+			return <TodoListTask title={task.title} isDone={task.isDone} priority={task.priority}/>
+		});
+
 		return (
 			<div className="todoList-tasks">
-				<div className="todoList-task">
-					<input type="checkbox" checked={true}/>
-					<span>CSS</span>
-				</div>
-				<div className="todoList-task">
-					<input type="checkbox" checked={false}/>
-					<span>JS</span>
-				</div>
-				<div className="todoList-task">
-					<input type="checkbox" checked={false}/>
-					<span>ReactJS</span>
-				</div>
-				<div className="todoList-task">
-					<input type="checkbox" checked={true}/>
-					<span>Patterns</span>
-				</div>
+				{todoListTasks}
 			</div>
 		)
 	}
