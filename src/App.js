489c5e243f535e5ff7cd90eb9400562e37d645c3
kotsur-vlad <kotsur.vlad@gmail.com>
2020-04-08 17:04:34 +0300

    add state, add "creating a new task", use method "setState"

--- src/App.js
+++ src/App.js
@@ -6,22 +6,44 @@ import TodoListTasks from "./TodoListTasks";
 
 class App extends React.Component {
 
-	tasks = [
-		{title: "JS", isDone: false, priority: "high"},
-		{title: "HTML", isDone: true, priority: "high"},
-		{title: "CSS", isDone: false, priority: "high"},
-		{title: "React", isDone: false, priority: "high"}
-	];
+	newTaskTitleRef = React.createRef();
 
-	filterValue = "All";
+	state = {
+		tasks: [
+			{title: "JS", isDone: false, priority: "high"},
+			{title: "HTML", isDone: true, priority: "medium"},
+			{title: "CSS", isDone: false, priority: "medium"},
+			{title: "React", isDone: false, priority: "high"}
+		],
+		filterValue: "All"
+	};
+
+	onAddTaskClick = () => {
+		let newTaskText = this.newTaskTitleRef.current.value;
+		let newTask = {
+			title: newTaskText, isDone: false, priority: "high"
+		};
+		let newTasks = [...this.state.tasks, newTask];
+		this.setState ({
+			tasks: newTasks
+		});
+		this.newTaskTitleRef.current.value ="";
+	};
 
 	render = () => {
 		return (
 			<div className="App">
 				<div className="todoList">
-					<TodoListHeader/>
-					<TodoListTasks tasks={this.tasks}/>
-					<TodoListFooter filterValue={this.filterValue}/>
+					{/*<TodoListHeader/>*/}
+					<div className="todoList-header">
+						<h3 className="todoList-header__title">What to Learn</h3>
+						<div className="todoList-newTaskForm">
+							<input ref={this.newTaskTitleRef} type="text" placeholder="Type new task name"/>
+							<button onClick={this.onAddTaskClick}>Add</button>
+						</div>
+					</div>
+					<TodoListTasks tasks={this.state.tasks}/>
+					<TodoListFooter filterValue={this.state.filterValue}/>
 				</div>
 			</div>
 		);
